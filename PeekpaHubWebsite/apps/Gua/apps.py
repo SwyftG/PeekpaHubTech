5a6b43e96653c91038ed0cb097c46813d576ce99
gaoliang3 <gaoliang3@le.com>
2019-10-25 18:26:47 +0800

    init

---
+++ PeekpaHubWebsite/apps/Gua/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GuaConfig(AppConfig):
+    name = 'apps.Gua'
---
+++ PeekpaHubWebsite/config/helper.py
@@ -0,0 +1,3 @@
+# encoding: utf-8
+__author__ = 'lianggao'
+__date__ = '2019/10/25 6:01 PM'
